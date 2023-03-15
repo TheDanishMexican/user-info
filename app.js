099a8594413cf1e708b0a56802d354be0e8d98ab
TheDanishMexican <danieljchristjansen@gmail.com>
2023-03-15 14:55:24 +0100

    as

--- app.js
+++ app.js
@@ -3,22 +3,40 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
-    // Event Listeners
-    document.querySelector("#form-create").addEventListener("submit", createUser);
+  // Event Listeners
+  document.querySelector("#form-create").addEventListener("submit", createUser);
 }
 
 function createUser(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = this.name.value;
-    const title = this.title.value;
-    const mail = this.mail.value;
-    const image = this.image.value;
+  const name = this.name.value;
+  const title = this.title.value;
+  const mail = this.mail.value;
+  const image = this.image.value;
+  const age = this.age.value;
+  const userName = this.userName.value;
 
-    console.log(name);
-    console.log(title);
-    console.log(mail);
-    console.log(image);
+  console.log(name);
+  console.log(title);
+  console.log(mail);
+  console.log(image);
+  console.log(age);
+  console.log(userName);
 
-    // validate user info
+  validateUser(name, title, mail, image, userName, age);
+}
+
+function validateUser(name, title, mail, image, userName, age) {
+  if (
+    (name.length > 2 &&
+      title &&
+      ((mail.length > 6 && mail.length < 20) || userName) &&
+      image &&
+      age > 18) == true
+  ) {
+    document.querySelector("h4").textContent = "This is a valid user";
+    document.querySelector("h4").classList.add("greentext");
+  } else document.querySelector("h4").textContent = "This is an invalid user";
+  document.querySelector("h4").classList.add("redtext");
 }
